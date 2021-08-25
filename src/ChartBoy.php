e7f0be251bf8ffcaa5d7f23df15325647faea8aa
unknown <52136571+wolfgang-degroot@users.noreply.github.com>
2021-08-24 22:05:01 -0500

    starts and stops

--- src/ChartBoy.php
+++ src/ChartBoy.php
@@ -1,59 +1,64 @@
 <?php
 namespace ChartBoy;
 
-function linkStyle (string $location)
+function linkStyle(string $location)
 {
-	// This function soley links to a charts.css file,
-	// whether it be from a pre-done CDN, or a local file.
-	// 'shorthands' for jsdelivr and unpkg are included
-	// for simple deployment if that's what you would do anyways.
-	$link;
-	switch ($location) {
-		case "jsdelivr":
-			$link = "https://cdn.jsdelivr.net/npm/charts.css/dist/charts.min.css";
-			break;
-		case "unpkg":
-			$link = "https://unpkg.com/charts.css/dist/charts.min.css";
-			break;
-		default:
-			$link = $location;
-	}
-	return "<link rel='stylesheet' href='{$link}'>";
+    // This function soley links to a charts.css file,
+    // whether it be from a pre-done CDN, or a local file.
+    // 'shorthands' for jsdelivr and unpkg are included
+    // for simple deployment if that's what you would do anyways.
+    $link;
+    switch ($location) {
+        case "jsdelivr":
+            $link = "https://cdn.jsdelivr.net/npm/charts.css/dist/charts.min.css";
+            break;
+        case "unpkg":
+            $link = "https://unpkg.com/charts.css/dist/charts.min.css";
+            break;
+        default:
+            $link = $location;
+    }
+    return "<link rel='stylesheet' href='{$link}'>";
 }
 
 class ChartBoy
 {
-    // Variables
+    /* Variables */
     public $type;
     // Types: bar, column, area, line
     // TODO: radial, pie, radar, polar
-    public $data;    // Array of data in the ["label" => numericValue] where "label" is optional.
+    public $data; // Array of data in the ["label" => numericValue] where "label" is optional.
     public $caption; // The 'caption' of the chart. Optional.
     private $min;
-    private $max;    // The min/max of the chart is calculated automatically.
+    private $max; // The min/max of the chart is calculated automatically.
+
+    // charts.css variables
+    protected $hideData;
 
     public function __construct(array $data, string $type = "bar", $caption = null)
     {
-		$this->data = $data;
+        $this->data = $data;
         $this->type = $type;
-		$this->caption = $caption;
+        $this->caption = $caption;
 
-		$this->min = min($data);
+        $this->min = min($data);
         $this->max = max($data);
+
+        // charts.css
+        $this->hideData = false;
     }
 
-	// Logic
+    // Logic
 
+    private function makeScale($n)
+    {
+        // Calculate a percentage on a scale of 100.
+        // This is to hide exact values from the public,
+        // in case such information is sensitive.
+        return round(($n / $this->max), 3);
+    }
 
-	private function makeScale($n)
-	{
-		// Calculate a percentage on a scale of 100.
-		// This is to hide exact values from the public,
-		// in case such information is sensitive.
-		return round(($n / $this->max), 3);
-	}
-
-    // Setters
+    /* Setters */
 
     public function setCaption($caption)
     {$this->caption = $caption;}
@@ -61,9 +66,13 @@ class ChartBoy
     public function setType($type)
     {$this->type = $type;}
 
+    // charts.css
+    public function hideData($bool = true)
+    {$this->hideData = $bool;}
+
     public function inputData($array)
     {
-		// Takes in a new input data array and updates calculations regarding it.
+        // Takes in a new input data array and updates calculations regarding it.
         $this->data = $array;
         $this->min = min($array);
         $this->max = max($array);
@@ -73,24 +82,28 @@ class ChartBoy
 
     public function renderChart($startPoint = false)
     {
-        print("\n\t<table class='charts-css {$this->type}'>\n");
+        print("\n\t<table class='charts-css {$this->type}'>");
+        if (!empty($this->caption)) {print("\n\t<caption>{$this->caption}</caption>");}
+        print("\n\t<tbody>\n");
         foreach ($this->data as $item => $value) {
-			// Loop through each data item
+            // Loop through each data item
 
-			$current = $this->makeScale($value);
-            $next    = $this->makeScale(next($this->data));
+            $current = $this->makeScale($value);
+            $next = $this->makeScale(next($this->data));
 
-			// Does this chart require a starting point?
-			if ($startPoint) {
-				$start = "--start:{$current} ";
-				$size  = "--size:{$next}";
-			} else {
-				$start = null;
-				$size  = "--size:{$current}";
-			}
+            // Does this chart require a starting point?
+            if ($startPoint) {
+                $start = "--start:{$current};";
+                $size = "--size:{$next}";
+                if ($item === array_key_last($this->data)) {break;}
+            } else {
+                $start = null;
+                $size = "--size:{$current}";
+            }
 
-            print("\t\t<td style='{$start}{$size}'>{$item}</td>\n");
+            print("\t\t<tr><td style='{$start}{$size}'><span class='data'>{$item}</span></td></tr>\n");
         }
+        print("\t</tbody>\n\t");
         print("\t</table>\n\t");
     }
 }
